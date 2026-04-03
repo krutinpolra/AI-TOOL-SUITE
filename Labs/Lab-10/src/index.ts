468ec6f86bac19b8329de761c35a2c9a3fbecf3e
kbpolra <kbpolra@myseneca.ca>
2026-04-03 19:31:04 -0400

    feat: add types and function stubs

---
+++ Labs/Lab-10/src/index.ts
@@ -0,0 +1,4 @@
+export { encodeFile, encodeBuffer } from './encode';
+export { parseDataURI, decodeToBuffer, decodeToFile } from './decode';
+export { MediaTypeSchema, DataURISchema, EXTENSION_TO_MIME, getCategory } from './types';
+export type { MediaType, MediaCategory, DataURI } from './types';
